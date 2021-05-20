da65c53e18007f8fb363d9731d0671a5bac711b3
halfkiwi90 <happykiwi90@gmail.com>
2021-05-20 15:42:12 +0900

    ✨ Event Component with fake events

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,5 +1,5 @@
-import { DateTime } from 'luxon';
-
+import { DateTime, Interval } from 'luxon';
+import {events} from '../fakeData/Events';
 const timeToPixel = (time:DateTime):number=>{
   const minute = time.minute;
   const hour = time.hour;
@@ -20,25 +20,50 @@ const Redline = ({height}:any)=>{
 
 }
 
-const Day = ({isToday}:any) => {
-  // const now = DateTime.now()   //  현재시간
-  const tenOClock = DateTime.local(2021, 5, 19, 10, 40);  //임의 시간 지정
-
-  const height = timeToPixel(tenOClock)
-  console.log(isToday)
+const Event = ({ event }: any) => {
+  const start = DateTime.fromISO(event.startTime);
+  const end = DateTime.fromISO(event.endTime);
+  const startPixel = timeToPixel(start);
+  const interval = Interval.fromDateTimes(start, end);
+  const intervalPixel = 48 * interval.length("hour");
+  const position = {
+    top: startPixel,
+    height: intervalPixel,
+    backgroundColor: event.colour
+  };
   return (
-    <div className = "day-con">
-      {
-        isToday 
-        ? <Redline height={height}/>
-        : null
-      }
-      <div className = "day-con-1">
-        <div className = "event-todo-con">  {/* 할일, 이벤트 컴포넌트 들어갈곳 */} 
+    <div className="event-con" style={position}>
+      <div className="event-text-con">
+        {/* 30분짜리면 span 1시간 이상이면 div 인데 */}
+        <div className="event-text-title-con">
+          <span className="event-text-title">{event.eventName}</span>
+        </div>
+        <div className="event-text-time">
+          {start.toFormat("HH:mm")+"~"+end.toFormat("HH:mm")}
         </div>
       </div>
     </div>
-  )
-  }
+  );
+};
+
+const Day = ({ dayNum }: any) => {
+  const now = DateTime.now(); //  현재시간
+  const height = timeToPixel(now);
+  const isToday = dayNum === now.day;
+  const dayEvent = events.filter((event) => {
+    return DateTime.fromISO(event.startTime).day === dayNum;
+  });
+  return (
+    <div className="day-con">
+      {isToday ? <Redline height={height} /> : null}
+      <div className="day-con-1">
+        {/* 할일, 이벤트 컴포넌트 들어갈곳 */}
+        {dayEvent.map((event) => (
+          <Event event={event} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Day;
