1a74c895071b4566a7016049b85e6a69f05e1122
Han Sam <hansam2yo@gmail.com>
2021-05-29 02:14:06 +0900

    [Client]/ #120-1 /💄  update calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route, Redirect, withRouter, BrowserRouter } from "react-router-dom";
-import Main from './pages/Main';
-import './style/Main.scss';
-import CreateCalendar from './pages/CreateCalendar';
-import RequestSubCalendar from './pages/RequestSubCalendar';
-import UpdateCalendar from './pages/UpdateCalendar';
-
+import React from "react";
+import "./App.css";
+import {
+  Switch,
+  Route,
+  Redirect,
+  withRouter,
+  BrowserRouter,
+} from "react-router-dom";
+import Main from "./pages/Main";
+import "./style/Main.scss";
+import CreateCalendar from "./pages/CreateCalendar";
+import RequestSubCalendar from "./pages/RequestSubCalendar";
+import UpdateCalendar from "./pages/UpdateCalendar";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/main'render={()=>( <Main/> )} />
-        <Route path='/setting/createcalendar' render={()=>( <RequestSubCalendar/> )} />
-        <Route path='/setting/requestsubcalendar'render={()=>( <RequestSubCalendar/> )} />
-        <Route path='/setting/updatecalendar'render={()=>( <UpdateCalendar/> )} />   
-        <Route path='/' render={() => {return <Redirect to='/main' />;}} />             
+        <Route path="/main" render={() => <Main />} />
+        <Route
+          path="/setting/createcalendar"
+          render={() => <CreateCalendar />}
+        />
+        <Route
+          path="/setting/requestsubcalendar"
+          render={() => <RequestSubCalendar />}
+        />
+        <Route
+          path="/setting/updatecalendar"
+          render={() => <UpdateCalendar />}
+        />
+        <Route
+          path="/"
+          render={() => {
+            return <Redirect to="/main" />;
+          }}
+        />
       </Switch>
     </BrowserRouter>
   );
